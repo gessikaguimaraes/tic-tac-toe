ea7db06dc31a3e34b915bc985132b0fac34c2350
Géssika <gessika.156@gmail.com>
2022-03-09 16:28:15 -0300

    Componenet display logic

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,17 +4,19 @@ import './Board.css';
 import { Cell } from '../Cell/Cell';
 
 export function Board() {
+    const cellValues = ['X', 'X', 'X', 'O', 'O', 'X', 'O', '', ''];
+    const winningCombination = [0, 3, 6];
+
+    const cells = cellValues.map((value, index) => {
+        const canHighlight = winningCombination &&
+            winningCombination.indexOf(index) >= 0;
+
+        return <Cell key={index} value={value} canHighlight={canHighlight} />
+    });
+
     return (
         <div id="board">
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
-            <Cell />
+            {cells}
         </div>
     );
 }
--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,10 +1,21 @@
 import React from 'react';
+//import classNames from 'classnames';
+
 import './Cell.css';
 
-export function Cell() {
+export const Cell = (props) => {
+    // const cellClasses = classNames({
+    //     cell: true,
+    //     winner: props.canHighlight
+    // });
+    // const cellContentClasses = classNames({
+    //     'cell-content': true,
+    //     populated: props.value
+    // });
+
     return (
         <button className="cell">
-            <span className="cell-content"></span>
+            <span className='cell-content populated'>{props.value}</span>
         </button>
     );
 }
